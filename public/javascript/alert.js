12d369fa08b33a96dfcaeee9cd06de002f4ef191
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-11-13 18:07:42 +0100

    gastion msg alert ok

--- public/javascript/alert.js
+++ public/javascript/alert.js
@@ -1,7 +1,7 @@
 const alerts = document.querySelectorAll(".alert");
 
 alerts.forEach((alert, index) => {
-  alert.style.transform = `translate(-50%, ${60 * index}px)`;
+  alert.style.transform = `translate(-50%, ${90 * index}px)`;
   if (alert) {
     setTimeout(() => {
       alert.remove();
